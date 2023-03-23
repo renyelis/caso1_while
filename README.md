96257c2e9368771aab2fae2932b06bbceaf3383b
juandavid28 <juanrodriguez1@colegioguanenta.edu.co>
2023-03-23 18:29:45 -0500

    se agrego el diagrama de flujo y el programa

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # caso1_while
 programa para hallar la suma de los primeros N numeros
+
+![Diagrama](diagrama.png "Diagrama de flujo")
---
+++ caso1.py
@@ -0,0 +1,14 @@
+# Caso1
+
+n = int(input("digite un numero: "))
+
+suma = 0
+
+i = 1
+while i<= n:
+    suma = suma+ i
+    i = i + 1
+
+
+print("la suma es " + str(suma))
+
